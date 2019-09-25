4e672d238df58f68e863b79a53dd222751176dae
Mar Mar Lwin <marmarlwin.dev@gmail.com>
2019-09-25 23:18:48 +0630

    add pattern

--- app/src/main/java/com/example/floristbypo/viewmodels/CatalogViewModel.kt
+++ app/src/main/java/com/example/floristbypo/viewmodels/CatalogViewModel.kt
@@ -3,13 +3,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.floristbypo.models.Catalog
-import com.example.floristbypo.models.User
 import com.example.floristbypo.repo.CatalogRepository
 
 //class CatalogViewModel(
 //    private val catalogRepository: CatalogRepository
 //): ViewModel() {
 class CatalogViewModel:ViewModel(){
-    private val _items = MutableLiveData<List<Catalog>>().apply { value = List<User() > }
+    private val _items = MutableLiveData<List<Catalog>>().apply { value = emptyList() }
     val items: LiveData<List<Catalog>> = _items
 }
